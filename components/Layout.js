1bfdf99fa866f381ad6a854e3de6624797dbaacd
AngeloBenavidez <benavidezangelo9@gmail.com>
2021-02-23 22:48:51 -0500

    Carousel agregado

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,3 @@
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 
-export default function Layout({children}){
-    return(
-        <div>
-           
-        </div>
-    )
-}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar'
+import Carousel from '../components/Carousel'
 
 
 const index = () => {
@@ -7,6 +8,9 @@ const index = () => {
     <div>
       <NavBar/>
       <h1>Hola mundo</h1>
+      <div style={{background:"blue"}} className="w-100">
+        <Carousel/>
+      </div>
       <Footer/>
      
     </div>
