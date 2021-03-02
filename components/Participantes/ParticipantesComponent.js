c6cc722b08cd002dbeb64590a4d9125613b7fa99
AngeloBenavidez <benavidezangelo9@gmail.com>
2021-03-02 10:50:25 -0500

    Agregar pagina participantes

---
+++ components/Participantes/ParticipantesComponent.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import { Grid, Card, Image, Icon } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
---
+++ pages/invitado/participantes/index.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { Spinner } from 'react-bootstrap';
+import CardParticipante from '../components/MainPage/CardParticipante';
+import axios from 'axios';
+
+export async function getServerSideProps() {
+  const res = await axios.get(process.env.apiURL + '/participantes');
+  const data = res.data;
+
+  return {
+    props: { data }, // se pasara la data automaticamente a la pagina como props
+  };
+}
+const index = ({ data }) => {
+  return (
+    <div>
+      <Layout>
+        <h2 style={{ margin: '2em' }}>Participantes</h2>
+        <CardParticipante data={data} />
+      </Layout>
+    </div>
+  );
+};
+
+export default index;
